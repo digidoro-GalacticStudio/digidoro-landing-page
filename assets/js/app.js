a67820118885eb7371fd19dd237016d4bd30efc7
cam-vasquez <00065221@uca.edu.sv>
2023-07-13 00:11:23 -0600

    DIGI-57 Add features

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 
 window.addEventListener('scroll', function(){
-    var header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 80);
 });
