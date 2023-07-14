fb58733040ab932654e2dc121c83ec44ddea852a
cam-vasquez <00065221@uca.edu.sv>
2023-07-14 09:41:12 -0600

    DIGI-75 add landing

--- digidoro/assets/js/app.js
+++ digidoro/assets/js/app.js
@@ -3,3 +3,34 @@ window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 80);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.menu');
+
+/* menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    menu.classList.toggle('open');
+}
+
+window.onclick = () =>{
+    menu.classList.remove('bx-x');
+    menu.classList.remove('open');
+}
+ */
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+          entry.target.classList.add('show');  
+        }
+        else{
+            entry.target.classList.remove('show');  
+
+        }
+
+    })
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
